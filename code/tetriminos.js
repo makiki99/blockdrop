8470265eda8426a08150fd63caedd5a1a6c7401f
makiki99 <mat.gawro@gmail.com>
2016-01-24 14:19:22 +0100

    added incomplete mino data and matrix array

---
+++ code/tetriminos.js
@@ -0,0 +1,73 @@
+var minoData = [
+  //one huge TODO
+  {
+    // s-mino
+    color: 1, //red
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+    rotationAllowed: true
+  },
+  {
+    // z-mino
+    color: 2, //green
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+    rotationAllowed: true
+  },
+  {
+    // j-mino
+    color: 3, //blue
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+    rotationAllowed: true
+  },
+  {
+    // l-mino
+    color: 4, //orange
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+    rotationAllowed: true
+  },
+  {
+    // i-mino
+    color: 5, //cyan
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+    rotationAllowed: true
+  },
+  {
+    // t-mino
+    color: 6, //purple
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+    rotationAllowed: true
+  },
+  {
+    // o-mino
+    color: 7, //yellow
+    spawnpos: [0,4],
+    rotation0: [[0,0],[0,1],[1,0],[1,1]],
+    rotation1: [[0,0],[0,1],[1,0],[1,1]],
+    rotation2: [[0,0],[0,1],[1,0],[1,1]],
+    rotation3: [[0,0],[0,1],[1,0],[1,1]],
+  },
+
+]
