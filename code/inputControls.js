606fc46757769d72d510169a525672abae5d60b0
makiki99 <mat.gawro@gmail.com>
2016-03-02 20:44:25 +0100

    Added in-game control scheme editor

---
+++ code/inputControls.js
@@ -0,0 +1,42 @@
+var isChangingKeyCode = false
+
+function keyCapture(e) {
+  if (e.keyCode != 27) {
+    controls.keyCodes[controls.currentSelection] = e.keyCode
+  }
+  keys[e.keyCode] = false
+  document.body.removeEventListener("keydown",keyCapture)
+  isChangingKeyCode = false
+}
+
+function inputControls() {
+  if (keys[38] && !isChangingKeyCode) {
+    //up arrow
+    controls.currentSelection--
+    if (controls.currentSelection < 0) {
+      controls.currentSelection = controls.keyCodes.length-1
+    }
+    keys[38] = false
+  }
+
+  if (keys[40] && !isChangingKeyCode) {
+    //down arrow
+    controls.currentSelection++
+    if (controls.currentSelection > controls.keyCodes.length-1) {
+      controls.currentSelection = 0
+    }
+    keys[40] = false
+  }
+
+  if (keys[13]) {
+    //enter
+    isChangingKeyCode = true
+    document.body.addEventListener("keydown",keyCapture)
+  }
+
+  if (keys[27]) {
+    //escape
+    gamestate = 0
+  }
+
+}
